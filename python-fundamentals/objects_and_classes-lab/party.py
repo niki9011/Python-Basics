f23f282c4c31a44405ad4e20734a752deda207bc
Nikolay Videnov <niki9011@abv.bg>
2022-10-28 00:30:22 +0300

    new task

---
+++ python-fundamentals/objects_and_classes-lab/party.py
@@ -0,0 +1,15 @@
+class Party:
+    def __init__(self):
+        self.names = []
+
+party = Party()
+while True:
+    name = input()
+
+    if name == "End":
+        break
+    else:
+        party.names.append(name)
+
+print(f"Going: {', '.join(party.names)}")
+print(f"Total: {len(party.names)}")
